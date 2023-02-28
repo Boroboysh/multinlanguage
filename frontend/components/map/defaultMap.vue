33f590d1642a692ba6dcfdc72435c0a5c5b1568c
kin_der_nature <kin_der_nature@mail.ru>
2023-02-28 16:04:45 +0500

    add: new component map

---
+++ frontend/components/map/defaultMap.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="default_map_block">
+    <div class="map_container w-100 d-flex align-items-center justify-content-center">
+      <img class="map_container_images map_container_map_state" src="@/assets/images/map/map_state.svg"
+        alt="">
+      <img class="position-absolute map_container_images map_container_map_kz" src="@/assets/images/map/map_kz.svg" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.map_block_content_title_block_col {
+  width: 50%;
+}
+
+
+.map_container_map_state {
+  max-height: 100%;
+  object-fit: contain;
+  width: 100%;
+  /* max-width: 1620px; */
+  z-index: -100;
+}
+
+.map_container_map_kz {
+  width: 68%;
+  object-fit: contain;
+  height: 100%;
+  z-index: -10;
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,54 +4,26 @@
     <div class="header_content_info container-xl">
       <div class="header_content_info row">
         <div class="row"></div>
-        <div
-          class="col header_content_info_col d-flex justify-content-center header_content_info_preview p-0"
-        >
+        <div class="col header_content_info_col d-flex justify-content-center header_content_info_preview p-0">
           <img src="@/assets/images/preview_header.png" alt="" />
         </div>
         <div class="col header_content_info_col header_content_info_forms">
           <h2 class="header_content_info_forms_title">Отследить</h2>
-          <default-input
-            placeholder="Поиск по коду груза"
-            class="header_content_info_forms"
-          />
-          <div
-            class="header_content_info_calculate_block w-100 d-block align-items-start flex-column"
-          >
+          <default-input placeholder="Поиск по коду груза" class="header_content_info_forms" />
+          <div class="header_content_info_calculate_block w-100 d-block align-items-start flex-column">
             <h2 class="header_content_info_forms_title">Рассчитать</h2>
-            <div
-              class="header_content_info_calculate_forms_block w-100 justify-content-between"
-            >
-              <default-input
-                class="header_content_info_calculate_form_input_size"
-                placeholder="Откуда"
-                :title="true"
-              />
-              <img
-                class="header_content_info_calculate_form_icons"
-                src="@/assets/images/arrow_double.svg"
-                alt=""
-              />
-              <default-input
-                class="header_content_info_calculate_form_input_size"
-                placeholder="Куда"
-                :title="true"
-              />
+            <div class="header_content_info_calculate_forms_block w-100 justify-content-between">
+              <default-input class="header_content_info_calculate_form_input_size" placeholder="Откуда" :title="true" />
+              <img class="header_content_info_calculate_form_icons" src="@/assets/images/arrow_double.svg" alt="" />
+              <default-input class="header_content_info_calculate_form_input_size" placeholder="Куда" :title="true" />
             </div>
-            <default-button
-              class="header_content_info_calculate_button"
-              size="large"
-            >
+            <default-button class="header_content_info_calculate_button" size="large">
               Рассчитать стоимость
             </default-button>
             <div
-              class="header_content_info_calculate_result w-100 d-flex justify-content-start justify-content-md-start justify-content-xl-center justify-content-xxl-end"
-            >
-              <img
-                class="header_content_info_calculate_result_icons d-none d-sm-block d-md-block d-xl-block"
-                src="@/assets/images/arrow_crocked.svg"
-                alt=""
-              />
+              class="header_content_info_calculate_result w-100 d-flex justify-content-start justify-content-md-start justify-content-xl-center justify-content-xxl-end">
+              <img class="header_content_info_calculate_result_icons d-none d-sm-block d-md-block d-xl-block"
+                src="@/assets/images/arrow_crocked.svg" alt="" />
               <span class="header_content_info_calculate_result_text">
                 От 2994 тенге, примерный срок 1—3 дня
               </span>
@@ -61,9 +33,7 @@
       </div>
       <div class="header_content_advantages_block">
         <div class="row">
-          <div
-            class="col-12 col-xl-4 col-md-4 header_content_advantages d-flex align-items-center justify-content-start"
-          >
+          <div class="col-12 col-xl-4 col-md-4 header_content_advantages d-flex align-items-center justify-content-start">
             <div class="header_content_advantages_info_block">
               <div class="header_content_advantages_number_block">
                 <span class="header_content_advantages_number">20</span>
@@ -76,9 +46,7 @@
               </div>
             </div>
           </div>
-          <div
-            class="col-xl-4 col-md-4 col-12 header_content_advantages d-flex align-items-center justify-content-start"
-          >
+          <div class="col-xl-4 col-md-4 col-12 header_content_advantages d-flex align-items-center justify-content-start">
             <div class="header_content_advantages_info_block">
               <div class="header_content_advantages_number_block">
                 <span class="header_content_advantages_number">20</span>
@@ -91,9 +59,7 @@
               </div>
             </div>
           </div>
-          <div
-            class="col-xl-4 col-md-4 header_content_advantages d-flex align-items-center justify-content-start"
-          >
+          <div class="col-xl-4 col-md-4 header_content_advantages d-flex align-items-center justify-content-start">
             <div class="header_content_advantages_info_block">
               <div class="header_content_advantages_number_block">
                 <span class="header_content_advantages_number">20</span>
@@ -146,25 +112,19 @@
               <span class="about_content_title">
                 Отделения ПЭК в Казахстане
               </span>
-              <div
-                class="about_content_block_info_adress_list_item d-flex align-items-center"
-              >
+              <div class="about_content_block_info_adress_list_item d-flex align-items-center">
                 <div class="about_content_block_info_adress_linear"></div>
                 <span class="about_content_block_info_adress_text">
                   Астана, ул. Жанажол, д. 19. Тел.: + 7 (7172) 644-644
                 </span>
               </div>
-              <div
-                class="about_content_block_info_adress_list_item d-flex align-items-center"
-              >
+              <div class="about_content_block_info_adress_list_item d-flex align-items-center">
                 <div class="about_content_block_info_adress_linear"></div>
                 <span class="about_content_block_info_adress_text">
                   Астана, ул. Жанажол, д. 19. Тел.: + 7 (7172) 644-644
                 </span>
               </div>
-              <div
-                class="about_content_block_info_adress_list_item d-flex align-items-center"
-              >
+              <div class="about_content_block_info_adress_list_item d-flex align-items-center">
                 <div class="about_content_block_info_adress_linear"></div>
                 <span class="about_content_block_info_adress_text">
                   Астана, ул. Жанажол, д. 19. Тел.: + 7 (7172) 644-644
@@ -183,15 +143,33 @@
     </div>
     <div class="map_block">
       <div class="map_block_content">
-        <div class="about_block_content_title_blockposition-relative d-flex">
+        <div class="map_block_title_block position-relative w-100 d-flex">
           <div class="map_block_linear about_block_content_title_linear" />
-          <div class="about_block_content_title_block container">
-            <span class="r about_block_content_title">
-              Куда компания ПЭК доставляет грузы?
-            </span>
+          <div class="map_block_content_title_block container-xl p-0">
+            <div class="map_block_content_title_block_row align-items-center justify-content-center w-100">
+              <div class="map_block_content_title_block_col map_content_block_title">
+                <span class="map_block_content_title">
+                  Куда компания ПЭК доставляет грузы?
+                </span>
+              </div>
+              <div class="map_block_content_title_block_col h-100 map_card_block">
+                <div class="map_block_content_card_block position-relative">
+                  <div class="default_border_block map_block_border_block">
+                    <div class="default_border default_border_top_left"></div>
+                    <div class="default_border default_border_top_right"></div>
+                    <div class="default_border default_border_bottom_left"></div>
+                    <div class="default_border default_border_bottom_right"></div>
+                  </div>
+                  <div class="map_block_content_card">
+                    Помогаем развивать бизнес наших клиентов, решая логистические задачи
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+      <default-map />
     </div>
   </div>
 </template>
@@ -201,6 +179,8 @@ import defaultButton from "@/components/button/defaultButton.vue";
 import defaultInput from "~~/components/input/defaultInput.vue";
 import defaultHeader from "@/components/header/defaultHeader/defaultHeader.vue";
 import DefaultHeader from "../components/header/defaultHeader/defaultHeader.vue";
+import defaultMap from "../components/map/defaultMap.vue";
+
 </script>
 <style>
 html,
@@ -212,9 +192,11 @@ body {
   font-family: 'Roboto';
   color: #2b2b2b;
 }
+
 .header_content_info_calculate_forms_block {
   display: flex;
 }
+
 .header_content_advantages_block {
   margin-top: 35px;
   margin-bottom: 120px;
@@ -228,6 +210,14 @@ body {
 .header_content_advantages_linear {
   width: 50%;
   height: 1px;
+}
+
+.default_linear_title_block {
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
 }
 
 .header_content_advantages_number_block {
@@ -298,8 +288,9 @@ body {
   align-items: center;
   width: 100%;
   height: 100%;
-  background: #fafafa;
+  /* background: #fafafa; */
 }
+
 .default_content_title_block {
   position: relative;
   display: flex;
@@ -324,9 +315,11 @@ body {
 .about_content_text {
   margin-top: 30px;
 }
+
 .about_content_info {
   max-width: 850px;
 }
+
 .about_block_content_advertisement {
   font-weight: bold;
   display: none;
@@ -334,43 +327,152 @@ body {
   border-left: 4px solid black;
   padding: 12px;
 }
+
 .about_content_block_info_adress_list_block {
   margin-top: 35px;
 }
+
 .about_content_block_info_adress_list_item {
   margin-top: 25px;
   font-size: 16px;
   color: #2b2b2b;
 }
+
 .about_content_block_info_adress_linear {
   width: 20px;
   height: 1px;
   background: #aaaaaa;
 }
+
 .about_content_block_info_adress_text {
   margin-left: 8px;
 }
+
 .map_block {
+  margin-top: 105px;
   width: 100%;
 }
+
+.map_content_block_title {
+  margin-top: 50px;
+  background: white;
+}
+
 .map_block_title {
   font-size: 48px;
 }
-.map_block_linear {
-  top: 25px;
-}
-/* .map_block_content_title {
 
-} */
+.map_block_border_block {
+  top: -25px;
+  left: -12px;
+}
+
+.map_block_linear {
+  top: 75px;
+}
+
+.map_card_block {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.map_block_content_card_block {
+  max-width: 500px;
+  min-height: 135px;
+}
+
+.map_block_content_title_block {
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  /* background: #ffffff; */
+}
+
+.map_block_content_title {
+  max-width: 700px;
+  font-size: 50px;
+  font-weight: 600;
+  line-height: 60px;
+}
+
+.map_block_content_title_block_col {
+  width: 50%;
+}
+
+.map_block_content_card {
+  padding: 20px 20px 40px;
+  font-size: 20px;
+  height: 100%;
+  background: #FAFAFA;
+}
+.map_block_content_title_block_row {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.map_card_block {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.default_border_block {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+.default_border {
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  /* background: #59b1ff; */
+}
+
+.default_border_top_left {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 40px;
+  height: 40px;
+  border-top: 1px solid #AAAAAA;
+  border-left: 1px solid #AAAAAA;
+}
+
+.default_border_top_right {
+  position: absolute;
+  top: 0;
+  right: 0;
+  border-top: 1px solid #AAAAAA;
+  border-right: 1px solid #AAAAAA;
+}
+
+.default_border_bottom_left {
+  bottom: 0;
+  left: 0;
+  border-bottom: 1px solid #AAAAAA;
+  border-left: 1px solid #AAAAAA;
+}
+
+.default_border_bottom_right {
+  bottom: 0;
+  right: 0;
+  border-bottom: 1px solid #AAAAAA;
+  border-right: 1px solid #AAAAAA;
+}
+
 @media (max-width: 1400px) {
-  .header_content_info_preview > img {
+  .header_content_info_preview>img {
     width: 100%;
     min-width: 320px;
   }
 }
 
 @media (max-width: 1000px) {
-  .header_content_info_preview > img {
+  .header_content_info_preview>img {
     width: 720px;
   }
 }
@@ -379,9 +481,11 @@ body {
   .header_logo_block {
     width: 30%;
   }
+
   .header_menu_block {
     width: 70%;
   }
+
   .header_content_info_calculate_form_input_size {
     min-width: 210px;
   }
@@ -391,8 +495,24 @@ body {
   .header_content_info_calculate_form_input_size {
     width: 100%;
   }
+
   .header_content_info_calculate_form_icons {
     margin: 15px;
+  }
+  .map_block_content_title_block_col {
+    margin-top: 0;
+  }
+  .map_block_content_title {
+    font-size: 30px;
+    margin-top: 60px;
+    max-width: 400px;
+    line-height: 30px;
+  }
+  .map_block_linear {
+    top: 50px;
+  }
+  .map_block_content_title_block {
+    margin-left: 20px;
   }
   /* .header_content_info_calculate_block {
     margin-top: 50px;
@@ -403,46 +523,83 @@ body {
   .header_content_advantages_info_block {
     display: block;
   }
+
   .header_content_info_calculate_block {
     margin-top: 50px;
   }
 }
 
-@media (max-width: 850px) {
+@media (max-width: 844px) {
+  .map_block_linear {
+    top: 60px;
+  }
 }
 
 @media (max-width: 780px) {
   .header_content_advantages_info_block {
     width: 100%;
   }
+
   .header_content_advantages_text {
     max-width: 320px;
   }
+
   .about_block_content_title {
     font-size: 20px;
   }
 }
+
+@media(max-width: 768px) {
+  .map_block_content_title_block_row {
+    display: flex;
+    flex-direction: column-reverse;
+  }
+  .map_block_content_title {
+    font-size: 30px;
+    max-width: 200px;
+    line-height: 30px;
+  }
+  .map_block_content_title_block_col {
+    width: 100%;
+    height: 100%;
+  }
+  .map_block_content_title {
+    width: 100px;
+  }
+  .map_block_linear {
+    top: 218px;
+  }
+  .map_content_block_title {
+    margin-top: 70px;
+  }
+}
+
 @media (max-width: 510px) {
   .header_content_info_calculate_form_input_size {
     min-width: auto;
   }
+
   .header_content_info_calculate_forms_block {
     display: block;
   }
+
   .header_content_info_calculate_result_text {
     margin: 0;
   }
+
   .header_content_info_calculate_form_icons {
     margin-left: 0;
   }
+
   .header_content_advantages_block {
     margin-bottom: 60px;
   }
+
   .about_block_content_title_block {
     display: none !important;
   }
+
   .about_block_content_advertisement {
     display: block;
   }
-}
-</style>
+}</style>
