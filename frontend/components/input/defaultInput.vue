8ae4177ae54d24214f1479ea64228effee84c995
kin-der-nature <johndoe@example.com>
2023-03-01 16:06:10 +0500

    fix(frontend): add default background

--- frontend/components/input/defaultInput.vue
+++ frontend/components/input/defaultInput.vue
@@ -49,6 +49,7 @@ const updateValue = (value) => {
   display: flex;
   align-items: center;
   position: relative;
+  background: white;
 }
 
 .default_input_block:focus-within {
@@ -87,7 +88,6 @@ const updateValue = (value) => {
 }
 
 .default_input_title {
-  /* width: 100%; */
   height: 100%;
   position: absolute;
   top: 0;
