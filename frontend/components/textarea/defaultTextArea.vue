89dd900baa8681df92e108d749c717050fc6c93f
kin-der-nature <johndoe@example.com>
2023-03-01 16:10:45 +0500

    add(frontend): new accrdion component

---
+++ frontend/components/textarea/defaultTextArea.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="default_text_area_block">
+    <textarea class="default_text_area"></textarea>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  placeholder: {
+    type: String,
+  },
+});
+</script>
+
+<style scoped>
+.default_text_area_block {
+  width: 100%;
+}
+.default_text_area {
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  border: 1px solid #e4e6e7;
+  outline: none;
+  padding: 20px;
+  font-size: 16px;
+  border-radius: 3px;
+  transition: 0.2s;
+  height: 100%;
+}
+.default_text_area:hover {
+  border: 1px solid #aaaaaa;
+}
+
+</style>
