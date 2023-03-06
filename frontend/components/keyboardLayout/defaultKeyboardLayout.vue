260d8adda4d6765d6f6ed0705d94ef44a2a52b9a
kin-der-nature <johndoe@example.com>
2023-03-06 18:55:38 +0500

    add(frontend): keyboard component

---
+++ frontend/components/keyboardLayout/defaultKeyboardLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="default_keyboard_layout_block">
+    <div class="default_keyboard_layout">
+        <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, provide } from 'vue'
+
+const props = defineProps({
+  typeAction: {
+    type: String,
+    requered: true
+  }
+})
+const result = ref(null)
+
+const choiceActions = (action, value) => {
+  switch (action) {
+    case 'default':
+      result = value
+  }
+}
+
+</script>
+
+<style scoped>
+.default_keyboard_layout {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
---
+++ frontend/components/keyboardLayout/defaultKeyboardOption.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="default_keyboard_option_block">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  value: {
+    type: String,
+    requered: true,
+  },
+});
+
+
+</script>
+
+<style scoped>
+.default_keyboard_option_block {
+    padding: 4px 8px;
+    margin-right: 4px;
+    background: #E4E6E7;
+    border-radius: 3px;
+}
+</style>
