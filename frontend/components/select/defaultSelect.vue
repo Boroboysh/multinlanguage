afaf0a10de16c5bdeab490705f2d7d9e555ab380
kin-der-nature <johndoe@example.com>
2023-03-06 18:56:28 +0500

    add(frontend): select component

--- frontend/components/select/defaultSelect.vue
+++ frontend/components/select/defaultSelect.vue
@@ -1,52 +1,72 @@
 <template>
-  <div class="default_select_block">
-    {{ isActive }}
-    <default-input @focus="udpateStatus(true)" @blur="udpateStatus(false)" v-model="value" :readonly="false"
-      :placeholder="placeholder">
+  <div class="default_select_block position-relative">
+    <default-input
+      @focus="udpateStatus(true)"
+      v-model="selectValue"
+      :readonly="false"
+      :placeholder="placeholder"
+    >
       <template #container-right>
-        <img class="default_select_search_icon_block" src="@/assets/images/arrow_done.svg" alt="">
+        <img
+          class="default_select_search_icon_block"
+          src="@/assets/images/arrow_done.svg"
+          alt=""
+        />
 
         <slot name="container-right" />
       </template>
     </default-input>
     <div v-if="isActive" class="default_select_list_block">
       <div class="default_select_list">
-        <div class="default_select_list_item">
-          1
-        </div>
-        <div class="default_select_list_item">
-          1
-        </div>
-        <div class="default_select_list_item">
-          1
-        </div>
+        <slot />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import defaultInput from '../input/defaultInput.vue';
-import { ref } from 'vue';
+import defaultInput from "../input/defaultInput.vue";
+import { ref, provide } from "vue";
 
 defineProps({
   placeholder: {
-    type: String
-  }
-})
+    type: String,
+  },
+});
 
-let value = ref('')
-let isActive = ref(false)
+let selectValue = ref("");
+let isActive = ref(false);
 
 const udpateStatus = (status) => {
   isActive.value = status;
+};
+
+const updateValue = (value) => {
+  console.log(value)
+  selectValue.value = value
 }
+
+const closeOptionBlock = () => {
+
+}
+
+provide('defaultSelect', {
+  updateValue,
+  udpateStatus
+})
+
 </script>
 
-<style>
+<style scoped>
+.default_select_block {
+}
 .default_select_list_block {
   width: 100%;
   padding: 10px;
+  position: absolute;
+  top: 100%;
+  background: #ffffff;
+  z-index: 1000;
   box-shadow: 0px 0px 12px rgba(95, 124, 151, 0.25);
 }
 </style>
