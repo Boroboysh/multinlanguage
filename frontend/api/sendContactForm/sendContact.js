b4c21f304def615e527cc0828aec19e581aab151
kin-der-nature <johndoe@example.com>
2023-03-16 04:39:41 +0500

    add(frontend): send contact forms

--- frontend/api/sendContactForm/sendContact.js
+++ frontend/api/sendContactForm/sendContact.js
@@ -1,6 +1,6 @@
 import httpCommon from "@/api/httpCommon/httpCommon";
 
 export const sendContact = (data) => {
-  const response = httpCommon.post('/send_email', data)
+  const response = httpCommon.post('/api/send_email', data)
   return response
 }
