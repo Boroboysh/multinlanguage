56d11e568c1698cae38c58349698b616497d412c
kin-der-nature <johndoe@example.com>
2023-03-15 12:38:51 +0500

    add(frontend): component marker

--- frontend/components/map/markers/defaultCityMarket.vue
+++ frontend/components/map/markers/defaultCityMarket.vue
@@ -1,11 +1,29 @@
 <template>
-  
+  <div class="default_city_market_block d-flex">
+    <img
+      src="@/assets/images/defaut_marker.svg"
+      alt=""
+      class="map_container_marker"
+    />
+    <div class="map_container_marker_name">
+      {{ name }}
+      <slot />
+    </div>
+  </div>
 </template>
 
-<script>
+<script setup>
+const props = defineProps({
+    item: {
+        type: Object
+    }
+})
 
 </script>
 
 <style>
-
+.map_container_marker_name {
+  font-size: 12px;
+  margin: 0px 0px 0px 8px;
+}
 </style>
