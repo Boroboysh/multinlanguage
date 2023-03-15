c388db59a06fff9ce21dbf55f8d4c8d01c227df9
kin-der-nature <johndoe@example.com>
2023-03-16 01:02:46 +0500

    add(frontend): home stores

--- frontend/stores/homeStores.js
+++ frontend/stores/homeStores.js
@@ -4,17 +4,33 @@ import getContentInfo from "@/api/contentInfo/getContentInfo";
 export const useContentPages = defineStore({
   id: 'page-content',
   state: () => ({
-    pageContent: {}
+    pageContent: {},
+    currentLang: 'kk'
   }),
   getters: {
     getLang: (state) => {
       return state.pageContent.header?.languages
+    },
+    getActiveLang: (state) => {
+      return state.getLang?.filter((lang) => lang.isDefault === 1)
     }
   },
   actions: {
     async getContent (lang) {
-     const response = await getContentInfo('ru');
+     const response = await getContentInfo(lang);
      this.pageContent = response
+    },
+    updateCurrentLang (lang) {
+      this.currentLang = lang;
     }
+    // getCurrentLang (list, lang) {
+    //   let result = {}
+    //   list.map((item, index) => {
+    //     if (item.isDefault > list[index + 1].isDefault) {
+    //       result = item
+    //     }
+    //   })
+    //   return result
+    // }
   }
 })
